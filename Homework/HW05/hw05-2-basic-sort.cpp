d054f9e44ca57a2c8d9e275a5d3568eff51324dd
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-14 22:52:06 +0700

    zcczxczxczxc

--- Homework/HW05/hw05-2-basic-sort.cpp
+++ Homework/HW05/hw05-2-basic-sort.cpp
@@ -21,3 +21,38 @@
     Output:
         112 91 65
 */
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Input[1] : \n");
+    scanf("%d", &num1);
+    
+    printf("Input[2] : \n");
+    scanf("%d", &num2);
+
+    printf("Input[3] : \n");
+    scanf("%d", &num3);
+
+    if (num1 < num2) {
+        swap(&num1, &num2);
+    }
+    if (num2 < num3) {
+        swap(&num2, &num3);
+    }
+    if (num1 < num2) {
+        swap(&num1, &num2);
+    }
+
+    printf("Output: %d %d %d", num1, num2, num3);
+
+    return 0;
+}
